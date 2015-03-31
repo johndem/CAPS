4eda1b44627ff5a5acdcaa5003f45eda96b4289d
John Dematis <johndem13@gmail.com>
2015-03-31 20:24:31 +0300

    Added organisation registration functionality

--- create-link.php
+++ create-link.php
@@ -1,12 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "admin";
-$password = "caps";
-$dbname = "caps";
+    $servername = "localhost";
+    $username = "admin";
+    $password = "caps";
+    $dbname = "caps";
 
-$link = mysqli_connect($servername, $username, $password, $dbname);
+    $link = mysqli_connect($servername, $username, $password, $dbname);
 
-if (!$link) {
- die("Connection failed");
-}
+    if (!$link) {
+        die("Connection failed");
+    }
 ?>
---
+++ org-register.php
@@ -0,0 +1,37 @@
+<?php
+
+    include 'create-link.php';
+    $user = mysqli_real_escape_string($link,$_POST['user']);
+    $user = htmlspecialchars($user, ENT_QUOTES);
+
+    $email = mysqli_real_escape_string($link,$_POST['email']);
+    $email = htmlspecialchars($email, ENT_QUOTES);
+
+    $pass = mysqli_real_escape_string($link,$_POST['pass']);
+    $pass = htmlspecialchars($pass, ENT_QUOTES);
+
+    $con_pass = mysqli_real_escape_string($link,$_POST['con-pass']);
+    $con_pass = htmlspecialchars($con_pass, ENT_QUOTES);
+
+    $name = mysqli_real_escape_string($link,$_POST['name']);
+    $name = htmlspecialchars($name, ENT_QUOTES);
+
+    $website = mysqli_real_escape_string($link,$_POST['website']);
+    $website = htmlspecialchars($website, ENT_QUOTES);
+
+    $facebook = mysqli_real_escape_string($link,$_POST['facebook']);
+    $facebook = htmlspecialchars($facebook, ENT_QUOTES);
+
+    $twitter = mysqli_real_escape_string($link,$_POST['twitter']);
+    $twitter = htmlspecialchars($twitter, ENT_QUOTES);
+
+    $other = mysqli_real_escape_string($link,$_POST['other']);
+    $other = htmlspecialchars($other, ENT_QUOTES);
+
+    $description = mysqli_real_escape_string($link,$_POST['description']);
+    $description = htmlspecialchars($description, ENT_QUOTES);
+
+    mysqli_query($link,$query);
+    echo "<script> window.open('confirm.html','_self')</script>";
+
+?>

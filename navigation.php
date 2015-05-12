4805d64629729fc43ef446affc5d91174e29768d
John Dematis <johndem13@gmail.com>
2015-05-12 16:37:57 +0300

    Fixed selected tab bug

--- navigation.php
+++ navigation.php
@@ -1,12 +1,53 @@
 <?php
 
+$url = $_SERVER["REQUEST_URI"];
+
 echo '<div class="navigation">';
     echo '<ul>';
-        echo '<li><a href="index.php">HOME</a></li>';
-        echo '<li><a href="volunteers.php">VOLUNTEERS</a></li>';
-        echo '<li><a href="organisations.php">ORGANISATIONS</a></li>';
-        echo '<li><a href="calendar.php">CALENDAR</a></li>';
-        echo '<li><a href="account.php">ACCOUNT</a></li>';
+
+if (strpos($url,'index') !== false) {
+    echo '<li id="selected"><a href="index.php">HOME</a></li>';
+    echo '<li><a href="volunteers.php">VOLUNTEERS</a></li>';
+    echo '<li><a href="organisations.php">ORGANISATIONS</a></li>';
+    echo '<li><a href="calendar.php">CALENDAR</a></li>';
+    echo '<li><a href="account.php">ACCOUNT</a></li>';
+}
+else if ((strpos($url,'volunteers') !== false) || (strpos($url,'search-results') !== false) || (strpos($url,'event') !== false)) {
+    echo '<li><a href="index.php">HOME</a></li>';
+    echo '<li id="selected"><a href="volunteers.php">VOLUNTEERS</a></li>';
+    echo '<li><a href="organisations.php">ORGANISATIONS</a></li>';
+    echo '<li><a href="calendar.php">CALENDAR</a></li>';
+    echo '<li><a href="account.php">ACCOUNT</a></li>';
+}
+else if (strpos($url,'organisations') !== false) {
+    echo '<li><a href="index.php">HOME</a></li>';
+    echo '<li><a href="volunteers.php">VOLUNTEERS</a></li>';
+    echo '<li id="selected"><a href="organisations.php">ORGANISATIONS</a></li>';
+    echo '<li><a href="calendar.php">CALENDAR</a></li>';
+    echo '<li><a href="account.php">ACCOUNT</a></li>';
+}
+else if (strpos($url,'calendar') !== false) {
+    echo '<li><a href="index.php">HOME</a></li>';
+    echo '<li><a href="volunteers.php">VOLUNTEERS</a></li>';
+    echo '<li><a href="organisations.php">ORGANISATIONS</a></li>';
+    echo '<li id="selected"><a href="calendar.php">CALENDAR</a></li>';
+    echo '<li><a href="account.php">ACCOUNT</a></li>';
+}
+else if (strpos($url,'account') !== false) {
+    echo '<li><a href="index.php">HOME</a></li>';
+    echo '<li><a href="volunteers.php">VOLUNTEERS</a></li>';
+    echo '<li><a href="organisations.php">ORGANISATIONS</a></li>';
+    echo '<li><a href="calendar.php">CALENDAR</a></li>';
+    echo '<li id="selected"><a href="account.php">ACCOUNT</a></li>';
+}
+else {
+    echo '<li><a href="index.php">HOME</a></li>';
+    echo '<li><a href="volunteers.php">VOLUNTEERS</a></li>';
+    echo '<li><a href="organisations.php">ORGANISATIONS</a></li>';
+    echo '<li><a href="calendar.php">CALENDAR</a></li>';
+    echo '<li><a href="account.php">ACCOUNT</a></li>';
+}
+
     echo '</ul>';
 echo '</div>';
 

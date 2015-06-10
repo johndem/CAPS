3dd1e4fe4fd4050d071df3e46a1a65b326063071
John Dematis <johndem13@gmail.com>
2015-06-10 14:00:28 +0300

    Added functionality to quick search widget

--- quick-search-widget.php
+++ quick-search-widget.php
@@ -3,34 +3,39 @@
     <h3>Quick Search</h3>
 	
 	<div>
+        
         <div id="quick-selects">
-            <select id="select-categories">
+            <form action="search-results.php" method="get" >
+            <select name="category" id="select-categories">
                 <option selected="selected" value="0">Select category</option>
-                <option value="1">Education</option>
-                <option value="2">Healthcare</option>
-                <option value="3">Environment</option>
-                <option value="4">Animals</option>
-                <option value="5">Emergency</option>
-                <option value="6">Communities</option>
+                <option>Education</option>
+                <option>Healthcare</option>
+                <option>Environment</option>
+                <option>Animals</option>
+                <option>Emergency</option>
+                <option>Communities</option>
             </select>
             <h3>...And/Or</h3>
-            <select id="area">
+            <select name="areas" id="area">
                 <option value="0" selected>Select area</option>
-                <option value="1">Ανω Τούμπα</option>
-                <option value="2">Αμπελόκηποι Θεσσαλονίκης</option>
-                <option value="3">Ασβεστοχώρι</option>
-                <option value="4">Βάρνα</option>
-                <option value="5">Κωνσταντινοπολίτικα</option>
-                <option value="6">Σαράντα Εκκλησιές</option>
-                <option value="7">Κάτω Τούμπα</option>
-                <option value="8">Καλαμαριά</option>
-                <option value="9">Πυλαία</option>
+                <option>Ανω Τούμπα</option>
+                <option>Αμπελόκηποι Θεσσαλονίκης</option>
+                <option>Ασβεστοχώρι</option>
+                <option>Βάρνα</option>
+                <option>Κωνσταντινοπολίτικα</option>
+                <option>Σαράντα Εκκλησιές</option>
+                <option>Κάτω Τούμπα</option>
+                <option>Καλαμαριά</option>
+                <option>Πυλαία</option>
             </select>
+                
+            <div id="go">
+                <input type="submit" class="submitBtn" id="sButton" name="submit" value="Go" />
+            </div>    
+            
+            </form>
         </div>
         
-        <div id="go">
-            <input type="submit" class="submitBtn" onclick="checkform()" id="sButton" name="submit" value="Go" />
-        </div>
     </div>
 	
 </div>

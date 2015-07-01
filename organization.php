d75df4a0de194ef04195e534d8754f7fefbea2cf
John Dematis <johndem13@gmail.com>
2015-07-01 21:13:32 +0300

    Fixed homepage bug and first attempt at org info

---
+++ organization.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>GETTING STARTED WITH BRACKETS</title>
+        <meta name="description" content="An interactive getting started guide for Brackets.">
+        <link rel="stylesheet" href="main.css">
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="jq.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js"></script>
+    </head>
+    <body>
+		
+        
+            <!-- registration or username -->
+            <?php include 'log-state.php'; ?>
+
+            <!-- masthead -->
+            <?php include 'masthead.php'; ?>
+
+            <!-- navigation -->
+            <?php include 'navigation.php'; ?>
+
+            <!-- content -->
+            <div class="content">
+                <?php include 'find-org.php'; ?>
+                <h1 class="center-title"><?php echo "$org[4]"; ?></h1>
+
+                <div id="organization-page"> 
+                        
+                        <?php
+                        echo '<div class="side-widgets">';
+                        include 'recent-events-widget.php'; 
+                        include 'quick-search-widget.php';
+				        //include 'most-recent-event-widget.php';
+                        echo '</div>';
+                        ?>
+
+                        <div id="organization-info">
+                            
+                            <?php
+                                echo '<img src="' . $org[10] . '" width="200" height="200" />';
+                                echo '<div>';
+                                if ($org[5] != "") {
+                                    echo '<h3><a href="' . $org[5] . '">Visit our Website</a></h3>';
+                                }
+                                if ($org[6] != "") {
+                                    echo '<h3><a href="' . $org[6] . '">Find us on Facebook</a></h3>';
+                                }
+                                if ($org[7] != "") {
+                                    echo '<h3><a href="' . $org[7] . '">Find us on Twitter</a></h3>';
+                                }
+                                if ($org[8] != "") {
+                                    echo '<h3><a href="' . $org[8] . '">Other</a></h3>';
+                                }
+                                echo '<div id="org-desc">' . $org[9] . '</div>';
+                                echo '</div>';
+                                
+                            ?>
+
+                    </div>
+                
+                
+                <div id="event-blanket">
+                
+                </div>
+
+            </div>
+
+            <!-- footer -->
+            <?php include 'footer.php'; ?>
+        
+    </body>
+</html>

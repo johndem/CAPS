0e7fa7085f2bf77037e62ce8f98179feae6f5225
John Dematis <johndem13@gmail.com>
2015-09-05 23:41:46 +0300

    Fixed recent events widget bug

--- recent-events-widget.php
+++ recent-events-widget.php
@@ -1,24 +1,25 @@
-<div class="recent-events"> 
-<h4>Events you might be interested in!</h4>
-	
-	<?php 
+<?php 
 	
 	include 'create-link.php';
 	
-	$query = "SELECT id,title,category,area,image FROM events WHERE image<>'' ORDER BY RAND() LIMIT 2";
-	
+	$query = "SELECT id,title,category,area,image FROM events WHERE status = '1' AND image<>'' ORDER BY RAND() LIMIT 2";
 	$results = mysqli_query($link,$query);
+
+    if (mysqli_num_rows($results) > 0) {
+        echo '<div class="recent-events"> ';
+        echo '<h4>Events you might be interested in!</h4>';
+
+        while ($row = mysqli_fetch_row($results)) {
+            echo '<div class="event-info" onclick="window.location = '. "'event.php?id=$row[0]'" . '">'   ;
+            echo '<div class="wid-image" style="background-image: url(' . "'" . $row[4] . "'" .  ');">';
+            echo '</div>';
+            echo '<div class="events-wid-title">' . $row[1] . '</div>';
+            echo '<span><strong>Category:</strong> ' . $row[2] . '</span>';
+            echo '<span><strong>At: </strong>' . $row[3] .'</span>';
+            echo '</div>';
+        }
+
+        echo '</div>';
+    }
 	
-	while ($row = mysqli_fetch_row($results)) {
-		echo '<div class="event-info" onclick="window.location = '. "'event.php?id=$row[0]'" . '">'   ;
-		echo '<div class="wid-image" style="background-image: url(' . "'" . $row[4] . "'" .  ');">';
-		echo '</div>';
-		echo '<div class="events-wid-title">' . $row[1] . '</div>';
-		echo '<span><strong>Category:</strong> ' . $row[2] . '</span>';
-		echo '<span><strong>At: </strong>' . $row[3] .'</span>';
-		echo '</div>';
-	}
-	
-	?>
-	
-</div>
+?>

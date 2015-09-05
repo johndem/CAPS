33473a6abc844cfa51844831834d3bc0d0b31f2d
John Dematis <johndem13@gmail.com>
2015-09-05 13:19:43 +0300

    Fixed confirm.php

--- confirm.php
+++ confirm.php
@@ -1,57 +1,64 @@
 <!DOCTYPE html>
 <html>
 
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>TEAM THESSALONIKI VOLUNTEER NETWORK</title>
-        <meta name="description" content="An interactive getting started guide for Brackets.">
-        <link rel="stylesheet" href="main.css">
-        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-        <script src="jq.js"></script>
-    </head>
-    <body>
+	<head>
+		<meta charset="utf-8">
+		<!-- <meta name="google-signin-scope" content="https://www.googleapis.com/auth/plus.login" />
+		<meta name="google-signin-requestvisibleactions" content="http://schema.org/AddAction" />
+		<meta name="google-signin-cookiepolicy" content="single_host_origin" />
+		<meta name="google-signin-clientid" content="833122714001-dpnlqanm3n5pkeqme6q0tl6pppo5uq67.apps.googleusercontent.com" /> -->
 
-        <?php include 'log-state.php'; ?>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>TEAM THESSALONIKI VOLUNTEER NETWORK</title>
+		<meta name="description" content="An interactive getting started guide for Brackets.">
+		<link rel="stylesheet" href="main.css">
+		<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
+		<!-- <script src="https://apis.google.com/js/client:platform.js?onload=api" async defer></script> -->
+		<script src="googleplus2.js"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<script src="https://apis.google.com/js/api:client.js?onload=startApp"></script>
+
+		<script src="jq.js"></script>
+		<script src="form-check.js"></script>
+	</head>
+	<body>
+        <?php 
+        session_start();
+		if (isset($_SESSION['user'])) {
+			header("Location: index.php");
+		}	
+        ?>
+
+		<!-- registration or username -->
+		<?php //include 'log-state.php'; ?>
+
+        <!-- navigation -->
+         <?php include 'navigation.php'; ?>
 
         <!-- masthead -->
         <?php include 'masthead.php'; ?>
 
-        <!-- navigation -->
-        <?php include 'navigation.php'; ?>
-
-            <!-- content -->
-            <div class="content">
-                <h1 class="center-title">Congratulations!</h1>
-
-                <p style="font-size:20px; margin-top: 50px;" class="aligner">Your account has been successfully created!</p>
-                <p style="font-size:20px;" class="aligner"> <a href="login.php" id="confirm-link">Login</a> to your account and get started.</p>
+        <!-- content -->
+        <div class="content">
+            
+            <h1 class="center-title"></h1>
+            <div class="page-title"> 
+                <div class="main-title">Congratulations!</div>  
+            </div>
+            
+            <p style="font-size:20px; margin-top: 50px;" class="aligner">Your account has been successfully created!</p>
+            <p style="font-size:20px;" class="aligner"> <a href="login.php" id="confirm-link">Login</a> to your account and get started.</p>
                 
-                <div id="blanket">
-                
-                </div>
-
+            <div id="blanket">
             </div>
 
-            <!-- footer -->
-            <div class="footer">
+        </div>
 
-                <div id="links">
-                    <a href="">Login</a>
-                    <a href="">Terms and Conditions</a>
-                    <a href="">Privacy Policy</a>
-                </div>
+        <!-- footer -->
+        <?php
+        include 'footer.php';
+        ?>
 
-                <div id="copyright">
-                    <p>Copyright &copy; 2015 JayTee. All Rights Reserved.</p>
-                </div>
-
-            </div>
-
-        
-
-    </body>
-
+	</body>
 
 </html>

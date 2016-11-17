2c72bdc62d89493581eef6cd79ff77c9a34ff3eb
Thanasis Tolikas <thantolikas@gmail.com>
2016-11-17 14:12:52 +0200

    Some small changes
    - Event page UI update
    - Hide fb credentials

--- back-end/fb-callback.php
+++ back-end/fb-callback.php
@@ -13,8 +13,8 @@ session_start();
 require_once __DIR__ . '/Facebook/autoload.php';
 
 $fb = new Facebook\Facebook([
-  'app_id' => '1647764365516493', // Replace {app-id} with your app id
-  'app_secret' => 'f23411f11c859c3657f90f1bd83f31e4',
+  'app_id' => 'xxxxxxxxxxxxxxxxxxxx', // Replace {app-id} with your app id
+  'app_secret' => 'xxxxxxxxxxxxxxxxxxxxxxxxxxx',
   'default_graph_version' => 'v2.2',
   ]);
 
